7bb67e6f257a88429f42cc84dd570948acbc3490
AmirGadami <Aghad051@uottawa.ca>
2024-07-10 17:04:12 -0400

    Add data_handling script

---
+++ src/processing/data_handling.py
@@ -0,0 +1,24 @@
+import os
+import pandas as pd
+import joblib
+from configs import config
+
+def load_data(file_name):
+    filePath = os.path.join(config.DATAPATH,file_name)
+    _data = pd.read_csv(filePath)
+    return _data
+
+
+def save_pipeline(pipline_to_save):
+    piplinePath = os.path.join(config.SAVE_MODEL_PATH,config.MODEL_NAME)
+    joblib.dump(pipline_to_save,piplinePath)
+    print(f"The model has been saved under the name {config.MODEL_NAME}")
+
+
+def save_pipeline():
+    piplinePath = os.path.join(config.SAVE_MODEL_PATH,config.MODEL_NAME)
+    model_loaded = joblib.load(piplinePath)
+    print("The model has been loaded")
+    return model_loaded
+
+
